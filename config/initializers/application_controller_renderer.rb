8cb42578020bda5eaf64363ede7b4f9752c2895b
sssd <takuyax0806@docomo.ne.jp>
2021-03-06 09:30:30 +0000

    finish_2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'df82ea90c5b5b539823d793bcba2fabde6224b9a63cf7354de897853ce041322519627d2d37be402b92d4e8fc6688db310f24d588a78814c73f67c1a9817167e'
+
