b1211782649f692863778a084a5aab702ac5f409
sssd <takuyax0806@docomo.ne.jp>
2021-03-06 04:57:48 +0000

    start_2-5

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htm
  get 'top' => 'homes#top'
   post 'todolists' => 'todolists#create'
+  get 'todolists' => 'todolists#index'
 end
